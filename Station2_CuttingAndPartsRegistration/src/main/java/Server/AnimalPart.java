59e8c25ed31f18dd4fab2e0e515df3a3b5a3e4ad
bibek344326-viaSTE <344326@via.dk>
2024-10-23 18:41:20 +0200

    Station2 Setup and DB Updates

---
+++ Station2_CuttingAndPartsRegistration/src/main/java/Server/AnimalPart.java
@@ -0,0 +1,22 @@
+package Server;
+
+public class AnimalPart {
+                  private int partId;
+    private String partType;
+    private double weight;
+
+    public AnimalPart(int partId, String partType, double weight) {
+        this.partId = partId;
+        this.partType = partType;
+        this.weight = weight;
+    }
+
+    public int getPartId() {
+        return partId;
+    }
+
+    @Override
+    public String toString() {
+        return "PartID: " + partId + ", Type: " + partType + ", Weight: " + weight;
+    }
+}
---
+++ Station2_CuttingAndPartsRegistration/src/test/java/org/example/station2_cuttingandpartsregistration/Station2CuttingAndPartsRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.station2_cuttingandpartsregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station2CuttingAndPartsRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

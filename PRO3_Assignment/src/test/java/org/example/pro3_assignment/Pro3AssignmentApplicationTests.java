58a83bb70e2fa9e2ce838c57587f134c4ee68ade
Bibek Chaudhary <161454254+bibek344326-viaSTE@users.noreply.github.com>
2024-10-23 18:20:26 +0200

    Initial Setup and Station 2 Server Client

---
+++ PRO3_Assignment/src/test/java/org/example/pro3_assignment/Pro3AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pro3_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pro3AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

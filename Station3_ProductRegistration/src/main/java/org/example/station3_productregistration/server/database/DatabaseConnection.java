4b213d548c82802d460ab3874034e641c8f7dfab
bibek344326-viaSTE <344326@via.dk>
2024-10-25 11:09:24 +0200

    updated station3

--- Station3_ProductRegistration/src/main/java/org/example/station3_productregistration/server/database/DatabaseConnection.java
+++ Station3_ProductRegistration/src/main/java/org/example/station3_productregistration/server/database/DatabaseConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/postgres?currentSchema=slaughterhousedb";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "rachu1997";
+    private static final String PASSWORD = "P@ssw0rd";
 
     private Connection connection;
 

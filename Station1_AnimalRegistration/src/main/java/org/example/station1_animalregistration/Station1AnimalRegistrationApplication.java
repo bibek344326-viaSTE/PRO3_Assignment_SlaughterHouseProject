dafc520d5ad1081a6db92638efe6d8e16c6d9144
RachuSharma <147827045+RachuSharma@users.noreply.github.com>
2024-10-25 10:27:26 +0200

    added station1

---
+++ Station1_AnimalRegistration/src/main/java/org/example/station1_animalregistration/Station1AnimalRegistrationApplication.java
@@ -0,0 +1,13 @@
+package org.example.station1_animalregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Station1AnimalRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Station1AnimalRegistrationApplication.class, args);
+    }
+
+}
---
+++ Station1_AnimalRegistration/src/test/java/org/example/station1_animalregistration/Station1AnimalRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.station1_animalregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Station1AnimalRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

13a2d548bdb210ec83b7c296c00e20b097c20ee1
Amin Aouina <91031103+silamin@users.noreply.github.com>
2024-10-28 03:34:35 +0100

    grpc step 2

--- Station2_CuttingAndPartsRegistration/src/main/java/org/example/station2_cuttingandpartsregistration/Station2CuttingAndPartsRegistrationApplication.java
+++ Station2_CuttingAndPartsRegistration/src/main/java/org/example/station2_cuttingandpartsregistration/Station2CuttingAndPartsRegistrationApplication.java
@@ -1,7 +1,9 @@
 package org.example.station2_cuttingandpartsregistration;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Station2CuttingAndPartsRegistrationApplication {
@@ -9,5 +11,15 @@ public class Station2CuttingAndPartsRegistrationApplication {
     public static void main(String[] args) {
         SpringApplication.run(Station2CuttingAndPartsRegistrationApplication.class, args);
     }
+    @Component
+    public class AppStartupRunner implements CommandLineRunner {
 
+        @Override
+        public void run(String... args) throws Exception {
+            // Keep the application running
+            synchronized (this) {
+                wait();
+            }
+        }
+    }
 }

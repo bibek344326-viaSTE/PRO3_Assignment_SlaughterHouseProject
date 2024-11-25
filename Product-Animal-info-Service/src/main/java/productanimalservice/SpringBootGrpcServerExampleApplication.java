39fb90eaf9c1b5a813f6bf463bf69ab060624039
Amin Aouina <amine.aouina.lp.4@gmail.com>
2024-11-25 21:27:48 +0100

    refactor

---
+++ Product-Animal-info-Service/src/main/java/productanimalservice/SpringBootGrpcServerExampleApplication.java
@@ -0,0 +1,13 @@
+package productanimalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGrpcServerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGrpcServerExampleApplication.class, args);
+	}
+
+}
---
+++ Product-Animal-info-Service/src/main/java/slaughterhouse/SlaughterhouseServiceImpl.java
@@ -0,0 +1,12 @@
+package slaughterhouse;
+
+import io.grpc.stub.StreamObserver;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlaughterhouseServiceImpl extends SlaughterhouseServiceGrpc.SlaughterhouseServiceImplBase {
+
+}
